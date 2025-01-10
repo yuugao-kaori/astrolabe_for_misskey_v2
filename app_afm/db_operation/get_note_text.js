6b61565c99236a307823080d7491c342b6edc635
yuugao-kaori <yuugaokaori@gmail.com>
2025-01-10 23:38:36 +0900

    chat_gptとの連接、関数の共通化

--- app_afm/db_operation/get_note_text.js
+++ app_afm/db_operation/get_note_text.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv';
 import pkg from 'pg';
+import { writeLog } from './create_logs.js';
 const { Client } = pkg;
+
 
 config();
 
@@ -27,6 +29,7 @@ async function getRandomDinnerText() {
 
         if (result.rows.length === 0 || !result.rows[0].value) {
             console.error('dinner_textが見つかりません');
+
             return null;
         }
 
@@ -48,9 +51,45 @@ async function getRandomDinnerText() {
     }
 }
 
+async function getRandomNoteText(key) {
+    let client = createDBClient();
+    let connected = false;
+    
+    try {
+        await client.connect();
+        connected = true;
 
+        const query = 'SELECT value FROM note_text WHERE key = $1';
+        const result = await client.query(query, [`${key}`]);
+
+        if (result.rows.length === 0 || !result.rows[0].value) {
+            const error_message = `${key}が見つかりません`;
+            await writeLog('error', 'breakfast', error_message, null, null);
+            return null;
+        }
+
+        const dinnerTexts = result.rows[0].value;
+        const randomIndex = Math.floor(Math.random() * dinnerTexts.length);
+        return dinnerTexts[randomIndex];
+
+    } catch (error) {
+        const error_message = `${key}取得中にエラーが発生: ${error.message}`;
+        await writeLog('error', 'breakfast', error_message, null, null);
+        return null;
+    } finally {
+        if (connected) {
+            try {
+                await client.end();
+            } catch (e) {
+                const error_message = `DB接続のクローズ中にエラーが発生: ${e.message}`;
+                await writeLog('error', 'breakfast', error_message, null, null);
+            }
+        }
+    }
+}
 
 
 export {
-    getRandomDinnerText
+    getRandomDinnerText,
+    getRandomNoteText
 };
--- app_afm/index.js
+++ app_afm/index.js
@@ -2,13 +2,16 @@ import { config } from 'dotenv';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { sendDM, createNote } from './create_note.js';
-import { getTrafficNews, getGigazin,getNazology, getPublickey,getGameSpark, getMultiFeed } from './get_feed.js';
-import { getRandomDinner } from './get_dinner.js';
+import { sendDM, createNote } from './misskey_operation/create_note.js';
+import { getMultiFeed } from './webpage_operation/get_feed.js';
+import { getRandomDinner } from './db_operation/get_dinner.js';
 import { resetHeatCounter } from './maintenance.js';
-import { getRandomDinnerText} from './get_note_text.js';
-import { updateMultiMemorandum } from './update_memorandum.js';
-import { getMemorandumDinnerText, getMultiMemorandum} from './get_memorandum.js';
+import { getRandomDinnerText, getRandomNoteText} from './db_operation/get_note_text.js';
+import { updateMultiMemorandum } from './db_operation/update_memorandum.js';
+import { getMemorandumDinnerText, getMultiMemorandum} from './db_operation/get_memorandum.js';
+import { updateMultiKVoperation, getMultiKVoperation } from './db_operation/multi_db_connection.js';
+import {connectWebSocket_hybrid} from './misskey_operation/connect_websocket.js';
+import { writeLog } from './db_operation/create_logs.js';
 import schedule from 'node-schedule';
 
 config();
@@ -20,95 +23,56 @@ const scheduleOptions = {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 let noteText;
 
-// JSONファイルを読み込む
-try {
-    const jsonContent = await readFile(join(__dirname, 'note_text.json'), 'utf8');
-    noteText = JSON.parse(jsonContent);
-} catch (error) {
-    console.error('note_text.jsonの読み込みに失敗しました:', error);
-    process.exit(1);
-}
+
 
 async function breakfast(){
     const breakfast_text = await getMemorandumDinnerText();
     if (breakfast_text == '満腹') {
-        console.log("朝食の投稿はありません");
+        const info_message = '朝食の投稿はありません';
+        await writeLog('info', 'breakfast', info_message, null, null);
         return;
     }
     else{
-        
         const message = `むぅ……なんだかお腹が空いてきました。\nあ、そうだ！昨日の『${breakfast_text}』を温め直しましょう＾＾`;
         const result = await createNote(message); 
     }  
 }
 
 async function morning_greeting() {
-    const greetings = noteText.morning_greeting;
-    const message = greetings[Math.floor(Math.random() * greetings.length)];
+    const message = await getRandomNoteText('morning_greeting');
     const result = await createNote(message);
-    
-    if (result) {
-        console.log("おはよう投稿の送信に成功しました");
-    } else {
-        console.error("おはよう投稿の送信に失敗しました");
-    }
+    const info_message = '朝の挨拶を実行';
+    await writeLog('info', 'morning_greeting', info_message, null, null);
 }
 
 
 async function night_greeting() {
-    const greetings = noteText.night_greeting;
-    const message = greetings[Math.floor(Math.random() * greetings.length)];
+    const message = await getRandomNoteText('night_greeting');
     const result = await createNote(message);
     
-    if (result) {
-        console.log("おやすみ投稿の送信に成功しました");
-    } else {
-        console.error("おやすみ投稿の送信に失敗しました");
+    const info_message = '夜の挨拶を実行';
+    await writeLog('info', 'night_greeting', info_message, null, null);
+}
+
+async function multi_feed(FeedURL) {
+
+    const News = await getMultiFeed(FeedURL,1);
+    const feed_text = noteText.feed_text;
+    const news_comment = await getRandomNoteText(feed_text);
+    // debug用 console.log(News);
+    const FeedResult = await getMultiMemorandum(FeedURL);
+    if (FeedResult == News[0].link) {
+        const FeedName = FeedURL.replace('https://', '').replace('.com', '').replace('.jp', '').replace('.co.jp', '').replace('/feed', '');
+        const info_message = `Feed(${FeedName})は前回の実行から更新されていません`;
+        await writeLog('info', 'multi_feed', info_message, null, null);
+        return;
     }
-}
+    await updateMultiMemorandum(FeedURL, `${News[0].link}`);
+    
+    const FeedName = FeedURL.replace('https://', '').replace('.com', '').replace('.jp', '').replace('.co.jp', '').replace('/feed', '');
+    const info_message = `Feed(${FeedName})の投稿を実行`;
+    await writeLog('info', 'multi_feed', info_message, null, null);
 
-async function feed_train() {
-    const News = await getTrafficNews(1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
-    const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
-    const result = await createNote(message);
-
-}
-
-async function feed_gigazin() {
-    const News = await getGigazin(1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
-    const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
-    const result = await createNote(message);
-}
-
-async function feed_gamespark() {
-    const News = await getGameSpark(1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
-    const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
-    const result = await createNote(message);
-}
-
-async function feed_nazology() {
-    const News = await getNazology(1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
-    const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
-    const result = await createNote(message);
-}
-
-async function feed_Publickey() {
-    const News = await getPublickey(1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
     const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
     const result = await createNote(message);
 }
@@ -121,69 +85,65 @@ async function think_Dinner() {
             const result = await createNote(message);
             const Note_Text = '満腹'
             await updateMultiMemorandum('dinner', Note_Text);
+            return;
         }
-        const Note_Text = await getRandomDinnerText();
+        const Note_Text = await getRandomNoteText(dinner_text);
         const message = `${Note_Text}\n\n今夜の献立はこちら！\n『${dinner}』`;
         // メモランダムテーブルにNote_Textを保存
         await updateMultiMemorandum('dinner', Note_Text);
         const result = await createNote(message);
-        if (result) {
-            console.log("献立の投稿に成功しました");
-        } else {
-            console.error("献立の投稿に失敗しました");
-        }
+        const info_message = '夕食の投稿を実行';
+        await writeLog('info', 'think_Dinner', info_message, null, null);
     } else {
-        console.error("献立の取得に失敗しました");
+        const error_message = '献立の取得に失敗しました';
+        await writeLog('error', 'think_Dinner', error_message, null, null);
     }
 }
 
-async function test() {
+async function test(test) {
     //const message = "テスト投稿です\n（ちょっとお邪魔しますｿｿｿｿ）";
     //const result = await createNote(message);
     //const result = await sendDM(message);
-    const FeedURL = 'https://automaton-media.com/feed/';
-    const News = await getMultiFeed(FeedURL,1);
-    const feed_text = noteText.feed_text;
-    const news_comment = feed_text[Math.floor(Math.random() * feed_text.length)];
-    // debug用 console.log(News);
-    const FeedResult = await getMultiMemorandum(FeedURL);
-    if (FeedResult == News[0].link) {
-        console.log("新着記事はありません");
-        return;
-    }
-    await updateMultiMemorandum(FeedURL, `${News[0].link}`);
-    
-    const message = `${news_comment}\n\n${News[0].title}\n${News[0].link}`;
-    // const result = await sendDM(message);
+    let message = test;
+    const result = await sendDM(message);
 }
 
 async function main() {
     try {
+        // WebSocket接続
+        await connectWebSocket_hybrid();
         // 投稿関連のスケジュール
         schedule.scheduleJob({scheduleOptions, rule: '0 7 * * *'}, morning_greeting);
         schedule.scheduleJob({scheduleOptions, rule: '30 7 * * *'}, breakfast);
-        schedule.scheduleJob({scheduleOptions, rule: '0 10 * * *'}, feed_gamespark);
-        schedule.scheduleJob({scheduleOptions, rule: '0 12 * * *'}, feed_nazology);
-        schedule.scheduleJob({scheduleOptions, rule: '0 14 * * *'}, feed_train);
-        schedule.scheduleJob({scheduleOptions, rule: '0 16 * * *'}, feed_gigazin);
-        schedule.scheduleJob({scheduleOptions, rule: '0 18 * * *'}, feed_Publickey);
+        schedule.scheduleJob({scheduleOptions, rule: '0 10 * * *'}, () => multi_feed('https://trafficnews.jp/feed'));
+        schedule.scheduleJob({scheduleOptions, rule: '0 12 * * *'}, () => multi_feed('https://gigazine.net/news/rss_2.0/'));
+        schedule.scheduleJob({scheduleOptions, rule: '0 14 * * *'}, () => multi_feed('https://nazology.kusuguru.co.jp/feed'));
+        schedule.scheduleJob({scheduleOptions, rule: '0 16 * * *'}, () => multi_feed('https://www.publickey1.jp/atom.xml'));
+        schedule.scheduleJob({scheduleOptions, rule: '0 18 * * *'}, () => multi_feed('https://www.gamespark.jp/rss20/index.rdf'));
         schedule.scheduleJob({scheduleOptions, rule: '0 19 * * *'}, think_Dinner);
+        schedule.scheduleJob({scheduleOptions, rule: '0 21 * * *'}, () => multi_feed('https://automaton-media.com/feed/'));
         schedule.scheduleJob({scheduleOptions, rule: '0 22 * * *'}, night_greeting);
+        schedule.scheduleJob({scheduleOptions, rule: '20 23 * * *'}, () => test('test'));
+
         // 毎日3時にheatカウンターをリセット
         schedule.scheduleJob('0 3 * * *', async () => {
             const result = await resetHeatCounter();
             if (result) {
-                console.log("heatカウンターのリセットに成功しました");
+                const info_message = `heatカウンターのリセットに成功しました`;
+                await writeLog('info', 'main', info_message, null, null);
             } else {
-                console.error("heatカウンターのリセットに失敗しました");
+                const error_message = `heatカウンターのリセットに失敗しました`;
+                await writeLog('error', 'main', error_message, null, null);
             }
         });
-        await test()
+        //await test()
         // 本番運用ではDMを送信する。sendDM("なんか起動したみたいですよ");
     
-        console.log("朝の挨拶スケジュールを設定しました");
+        console.log("起動しました");
     } catch (error) {
-        console.error(`エラーが発生しました: ${error.message}`);
+        const error_message = `エラーが発生しました: ${error.message}`;
+        console.error(error_message);
+        await writeLog('error', 'main', error_message, null, null);
     }
 }
 
